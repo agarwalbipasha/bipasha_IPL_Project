75b526419b95557d640d80a34b502aa5143a8270
Bipasha Agarwal <agarwalbipasha@gmail.com>
2021-08-22 17:29:57 +0530

    Refactoring 5th question

--- src/server/index.js
+++ src/server/index.js
@@ -1,82 +1,6 @@
 const CSVtoJSON = require("csvtojson");
 var ipl = require('./ipl');
 
-//Find the number of times each team won the toss and also won the match
-
-function numberOfTimesWonTossAndMatch(matches) {
-  return matches.reduce((numberOfTimesWonToss, match) => {
-    const winner = match["toss_winner"];
-    if (numberOfTimesWonToss.hasOwnProperty(winner) && winner == match.winner) {
-      numberOfTimesWonToss[winner] += 1;
-    } else if (
-      !numberOfTimesWonToss.hasOwnProperty(winner) &&
-      winner == match.winner
-    ) {
-      numberOfTimesWonToss[winner] = 1;
-    }
-    return numberOfTimesWonToss;
-  }, {});
-}
-
-//Find a player who has won the highest number of Player of the Match awards for each season
-
-function playerOfMatchInEachSeason(matches) {
-  let playerOfMatchInEachSeason = {};
-  let match;
-  for (let i = 0; i < matches.length; i++) {
-    match = matches.filter((x) => x["season"] == matches[i]["season"]);
-    playerOfMatchInEachSeason[matches[i]["season"]] = playerOfSeason(match);
-  }
-  return playerOfMatchInEachSeason;
-}
-
-function playerOfSeason(match) {
-  let count = 0;
-  let obj = {};
-  for (let i = 0; i < match.length; i++) {
-    count = match.filter(
-      (x) => x["player_of_match"] == match[i]["player_of_match"]
-    ).length;
-    obj[match[i]["player_of_match"]] = count;
-  }
-  const sortable = Object.entries(obj).sort(([, a], [, b]) => b - a);
-  return sortable[0][0];
-}
-
-//Find the strike rate of a batsman for each season
-
-function strikeRateOfABatsman(matches, deliveries) {
-  let strikeRateOfABatsman = []; //batsman : {season: strike rate}
-  let setOfBatsman = new Set(deliveries.map((x) => x.batsman));
-  let arrOfBatsman = Array.from(setOfBatsman);
-  let delivery, deliveryPerMatchId, strikeRate;
-  for (let i = 0; i < arrOfBatsman.length; i++) {
-    let obj = {};
-    let objBatsman = {};
-    delivery = deliveries.filter((x) => x["batsman"] == arrOfBatsman[i]);
-    for (let j = 0; j < delivery.length; j++) {
-      deliveryPerMatchId = delivery.filter(
-        (x) => x["match_id"] == delivery[j]["match_id"]
-      );
-      let id = delivery[j]["match_id"];
-      let noOfBalls = deliveryPerMatchId.length;
-      let totalRuns = deliveryPerMatchId
-        .map((x) => Number(x["total_runs"]))
-        .reduce((a, b) => a + b, 0);
-      strikeRate = ((totalRuns / noOfBalls) * 100).toFixed(2);
-      let season;
-      for (let m = 0; m < matches.length; m++) {
-        if (id == matches[m]["id"]) {
-          season = matches[m]["season"];
-        }
-      }
-      obj[season] = strikeRate;
-      objBatsman[delivery[j]["batsman"]] = obj;
-    }
-    strikeRateOfABatsman.push(objBatsman);
-  }
-  return strikeRateOfABatsman;
-}
 
 // Find the highest number of times one player has been dismissed by another player
 
@@ -147,8 +71,8 @@ CSVtoJSON()
     // console.log(matchesPerSeason(matches));
     // console.log(ipl.matchesWonPerTeamPerYear(matches));
     // console.log(ipl.getMatchesPlayedPerYear(matches));
-    // console.log(numberOfTimesWonTossAndMatch(matches));
-    // console.log(playerOfMatchInEachSeason(matches));
+    // console.log(ipl.numberOfTimesWonTossAndMatch(matches));
+    // console.log(ipl.playerOfMatchInEachSeason(matches));
     // console.log(ipl.getMatchesPlayedInAYear(matches, 2015));
     // console.log(ipl.arrayOfYears(matches));
 
@@ -158,8 +82,8 @@ CSVtoJSON()
         // console.log(deliveries);
         // console.log(matches);
         // console.log(ipl.runsConcededPerTeam(matches, deliveries, 2016));
-        console.log(ipl.topTenEconomicalBowlers(matches, deliveries, 2015));
-        // console.log(strikeRateOfABatsman(matches, deliveries));
+        // console.log(ipl.topTenEconomicalBowlers(matches, deliveries, 2015));
+        console.log(ipl.strikeRateOfABatsman(matches, deliveries));
         // console.log(matchID(matches));
         // console.log(bestEconomyInSuperOvers(matches, deliveries));
         // console.log(highestCountOfPlayerDismissal(deliveries));
--- src/server/ipl.js
+++ src/server/ipl.js
@@ -6,6 +6,10 @@ module.exports = {
   matchesWonPerTeamPerYear: matchesWonPerTeamPerYear,
   runsConcededPerTeam: runsConcededPerTeam,
   topTenEconomicalBowlers: topTenEconomicalBowlers,
+  numberOfTimesWonTossAndMatch: numberOfTimesWonTossAndMatch,
+  playerOfMatchInEachSeason: playerOfMatchInEachSeason,
+  playerOfSeason: playerOfSeason,
+  strikeRateOfABatsman: strikeRateOfABatsman
 };
 
 //Number of matches played per year for all the years in IPL.
@@ -119,3 +123,81 @@ function topTenEconomicalBowlers(matches, deliveries, year) {
     .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
   return Object.keys(sortable).slice(0, 10);
 }
+
+//Find the number of times each team won the toss and also won the match
+
+function numberOfTimesWonTossAndMatch(matches) {
+  return matches.reduce((numberOfTimesWonToss, match) => {
+    const winner = match["toss_winner"];
+    if (numberOfTimesWonToss.hasOwnProperty(winner) && winner == match.winner) {
+      numberOfTimesWonToss[winner] += 1;
+    } else if (
+      !numberOfTimesWonToss.hasOwnProperty(winner) &&
+      winner == match.winner
+    ) {
+      numberOfTimesWonToss[winner] = 1;
+    }
+    return numberOfTimesWonToss;
+  }, {});
+}
+
+//Find a player who has won the highest number of Player of the Match awards for each season
+
+function playerOfMatchInEachSeason(matches) {
+  let playerOfMatchInEachSeason = {};
+  let match;
+  for (let i = 0; i < matches.length; i++) {
+    match = matches.filter((x) => x["season"] == matches[i]["season"]);
+    playerOfMatchInEachSeason[matches[i]["season"]] = playerOfSeason(match);
+  }
+  return playerOfMatchInEachSeason;
+}
+
+function playerOfSeason(match) {
+  let count = 0;
+  let obj = {};
+  for (let i = 0; i < match.length; i++) {
+    count = match.filter(
+      (x) => x["player_of_match"] == match[i]["player_of_match"]
+    ).length;
+    obj[match[i]["player_of_match"]] = count;
+  }
+  const sortable = Object.entries(obj).sort(([, a], [, b]) => b - a);
+  return sortable[0][0];
+}
+
+//Find the strike rate of a batsman for each season
+
+function strikeRateOfABatsman(matches, deliveries) {
+  let strikeRateOfABatsman = [];
+  let setOfBatsman = new Set(deliveries.map((x) => x.batsman));
+  let arrOfBatsman = Array.from(setOfBatsman);
+  let delivery, deliveryPerMatchId, strikeRate;
+  for (let i = 0; i < arrOfBatsman.length; i++) {
+    let obj = {};
+    let objBatsman = {};
+    delivery = deliveries.filter((x) => x["batsman"] == arrOfBatsman[i]);
+    for (let j = 0; j < delivery.length; j++) {
+      deliveryPerMatchId = delivery.filter(
+        (x) => x["match_id"] == delivery[j]["match_id"]
+      );
+      let id = delivery[j]["match_id"];
+      let noOfBalls = deliveryPerMatchId.length;
+      let totalRuns = deliveryPerMatchId
+        .map((x) => Number(x["total_runs"]))
+        .reduce((a, b) => a + b, 0);
+      strikeRate = ((totalRuns / noOfBalls) * 100).toFixed(2);
+      let season;
+      for (let m = 0; m < matches.length; m++) {
+        if (id == matches[m]["id"]) {
+          season = matches[m]["season"];
+        }
+      }
+      obj[season] = strikeRate;
+      objBatsman[delivery[j]["batsman"]] = obj;
+    }
+    strikeRateOfABatsman.push(objBatsman);
+  }
+  return strikeRateOfABatsman;
+}
+
